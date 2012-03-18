442c2ab699ce40406130d23f1d8f5d8c47e5ef9e
Troy <Hoops021@gmail.com>
2012-03-18 07:39:08 -0400

    Util updated

--- js/util.js
+++ js/util.js
@@ -1,5 +1,19 @@
 var UTIL = {
-  v3 : function (x,y,z) {
-  return new THREE.Vertex(new THREE.Vector3(x,y,z));
-  }
+	v3 : function (x,y,z) {
+		return new THREE.Vertex(new THREE.Vector3(x,y,z));
+	},
+      
+	v3c : function(radius, theta, z){
+		this.radius = _r;
+		this.theta = _th;
+		this.z = _z;
+	
+		function ConvertToCartesian()
+		{
+			return new v3( radius*Math.cos(theta),
+							radius*Math.sin(theta),
+							z );
+		}
+	}
+
 };
