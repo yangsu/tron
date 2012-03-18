35f0133f1a3ab2f0021a7f743b0885fbc05373b3
Yang Su <suyang1+services@gmail.com>
2012-03-18 15:44:36 -0400

    fixed v3c to construct objects properly

--- js/util.js
+++ js/util.js
@@ -2,18 +2,18 @@ var UTIL = {
 	v3 : function (x,y,z) {
 		return new THREE.Vertex(new THREE.Vector3(x,y,z));
 	},
-      
-	v3c : function(radius, theta, z){
-		this.radius = _r;
-		this.theta = _th;
-		this.z = _z;
-	
-		function ConvertToCartesian()
-		{
-			return new v3( radius*Math.cos(theta),
-							radius*Math.sin(theta),
-							z );
-		}
+	v3c : function (radius, theta, z) {
+		var Coord = function(_r, _th, _z){
+			this.radius = _r;
+			this.theta = _th;
+			this.z = _z;
+		};
+		Coord.prototype.ConvertToCartesian = function() {
+				return new v3(this.radius*Math.cos(this.theta),
+											this.radius*Math.sin(this.theta),
+											this.z);
+		};
+		return new Coord(radius, theta, z);
 	}
 
 };
