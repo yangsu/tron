ce9e3701c09329b59f6656cc4674f7ef7eecc005
Troy <Hoops021@gmail.com>
2012-03-18 08:05:33 -0400

    Tunnel & Script updates

--- js/Tunnel.js
+++ js/Tunnel.js
@@ -2,8 +2,81 @@
  * @author Troy Ferrell
  */
 
-function Tunnel(){
+function Tunnel(scene){
 	
+	this.tunnelSegments = new Array();
+	this.numOfSegments = 30;
+	this.tunnelMaterial;
 	
+	// create new tunnel segments & add to array
+	var newTunnelSeg, newTunnelMesh, i;
+	this.tunnelMaterial = new THREE.MeshBasicMaterial({
+    							color: 0xFF0000,
+    							wireframe:true });
 	
+    for(i = 0; i < this.numOfSegments; i += 1) {
+        newTunnelSeg = new TunnelSegment(-i*10);
+        newTunnelMesh = new THREE.Mesh(newTunnelSeg.geometry, this.tunnelMaterial);
+        scene.add(newTunnelMesh);
+
+        this.tunnelSegments[i] = newTunnelMesh;
+    }
 }
+
+Tunnel.prototype.render = function(){	
+	// Move Tunnel Along
+	_.each(this.tunnelSegments, function (segment) {
+	    segment.position.z += 5;
+	});
+
+	// Add new Segment to tunnel
+    var newTunnelSeg = CreateTunnelSegment(-this.numOfSegments*10);
+
+    var newTunnelMesh = new THREE.Mesh(newTunnelSeg.geometry, this.tunnelMaterial);
+    scene.add(newTunnelMesh);
+
+    this.tunnelSegments.push(newTunnelMesh);
+}
+		
+function TunnelSegment(startZ)
+{
+    this.geometry = new THREE.Geometry();
+    this.geometry.dynamic = true;
+
+    var deltaTheta = Math.PI/6,
+        radius = 50,
+        faceCounter = 0,
+        depth = 10,
+        theta, face,
+        rcos, rsin, rcosd, rsind,
+        temp;
+        
+    // dynamically create quads for tunnel segment
+    for (theta = 0; theta < 2*Math.PI; theta += deltaTheta){
+        rcos = radius*Math.cos(theta);
+        rsin = radius*Math.sin(theta);
+        rcosd = radius*Math.cos(theta + deltaTheta);
+        rsind = radius*Math.sin(theta + deltaTheta);
+
+        // Create vertices for current quad in cylinder segment
+        this.geometry.vertices.push(UTIL.v3(rcos, rsin, startZ),
+                          UTIL.v3(rcos, rsin, startZ - depth),
+                          UTIL.v3(rcosd, rsind, startZ - depth),
+                          UTIL.v3(rcosd, rsind, startZ));
+
+        // Define normals to point inward
+        temp = faceCounter*4;
+        face = new THREE.Face4(temp + 3,
+                               temp + 2,
+                               temp + 1,
+                               temp);
+
+        this.geometry.faces.push(face);
+        faceCounter += 1;
+    }
+
+    this.geometry.computeFaceNormals();
+    this.geometry.computeVertexNormals();
+}
+	
+	
--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@
  */
 var camera, scene, renderer, geometry, material, mesh, segment;
 
-var tunnelSegmentMeshes, tunnelMeshIndex, radius, segments,
-    rings, numOfSegments, tunnelMaterial;
+var tunnel;
 
 init();
 animate();
@@ -16,72 +15,14 @@ function init() {
                                          10000);
     camera.position.z = 100;
     scene.add(camera);
-
-    tunnelSegmentMeshes = [];
-    radius = 50;
-    segments = 16;
-    rings = 16;
-    numOfSegments = 30;
-    tunnelMaterial = new THREE.MeshBasicMaterial({
-        color: 0xFF0000,
-        wireframe:true
-    });
-
-    var newTunnelSeg, newTunnelMesh, i;
-    for(i = 0; i < numOfSegments; i += 1) {
-        newTunnelSeg = CreateTunnelSegment(-i*10);
-        newTunnelMesh = new THREE.Mesh(newTunnelSeg, tunnelMaterial);
-        scene.add(newTunnelMesh);
-
-        tunnelSegmentMeshes[tunnelSegmentMeshes.length] = newTunnelMesh;
-    }
+    
+    tunnel = new Tunnel(scene);
+    tunnel.init();
 
     renderer = new THREE.CanvasRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(renderer.domElement);
-}
-
-
-function CreateTunnelSegment(startZ)
-{
-    var geo = new THREE.Geometry();
-    geo.dynamic = true;
-
-    var deltaTheta = Math.PI/6,
-        radius = 50,
-        faceCounter = 0,
-        depth = 10,
-        theta, face,
-        rcos, rsin, rcosd, rsind,
-        temp;
-    for (theta = 0; theta < 2*Math.PI; theta += deltaTheta){
-        rcos = radius*Math.cos(theta);
-        rsin = radius*Math.sin(theta);
-        rcosd = radius*Math.cos(theta + deltaTheta);
-        rsind = radius*Math.sin(theta + deltaTheta);
-
-        // Create vertices for current quad in cylinder segment
-        geo.vertices.push(UTIL.v3(rcos, rsin, startZ),
-                          UTIL.v3(rcos, rsin, startZ - depth),
-                          UTIL.v3(rcosd, rsind, startZ - depth),
-                          UTIL.v3(rcosd, rsind, startZ));
-
-        // Define normals to point inward
-        temp = faceCounter*4;
-        face = new THREE.Face4(temp + 3,
-                               temp + 2,
-                               temp + 1,
-                               temp);
-
-          geo.faces.push(face);
-          faceCounter += 1;
-    }
-
-    geo.computeFaceNormals();
-    geo.computeVertexNormals();
-
-    return geo;
 }
 
 function animate() {
@@ -91,22 +32,7 @@ function animate() {
 }
 
 function render() {
-    // remove first segment in array
-    if (tunnelSegmentMeshes[0].position.z < -100)
-        tunnelSegmentMeshes.shift();
-
-    // Move Tunnel Along
-    _.each(tunnelSegmentMeshes, function (segment) {
-        segment.position.z += 5;
-    });
-
-    // Add new Segment to tunnel
-    var newTunnelSeg = CreateTunnelSegment(-numOfSegments*10);
-
-    var newTunnelMesh = new THREE.Mesh(newTunnelSeg, tunnelMaterial);
-    scene.add(newTunnelMesh);
-
-    tunnelSegmentMeshes.push(newTunnelMesh);
+	tunnel.render();
 
     renderer.render(scene, camera);
 }
