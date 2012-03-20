4eebf6f022b0e34728d239e52740fb0f6dfc2b91
Yang Su <suyang1+services@gmail.com>
2012-03-20 17:26:58 -0400

    Wrapped code in document.ready

--- js/script.js
+++ js/script.js
@@ -1,52 +1,55 @@
 /**
  * @author Troy Ferrell & Yang Su
  */
-var camera, scene, renderer, geometry, material, mesh, segment;
+$(document).ready(function () {
+    var camera, scene, renderer,
+        tunnel, myPlayer;
 
-var tunnel, myPlayer;
+    function init() {
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(75,
+                                             window.innerWidth / window.innerHeight,
+                                             1,
+                                             10000);
+        camera.position.z = 100;
+        scene.add(camera);
 
-init();
-animate();
-function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75,
-                                         window.innerWidth / window.innerHeight,
-                                         1,
-                                         10000);
-    camera.position.z = 100;
-    scene.add(camera);
+        tunnel = new Tunnel(scene);
+        myPlayer = new Player(scene);
 
-    tunnel = new Tunnel(scene);
-    myPlayer = new Player(scene);
+        renderer = new THREE.CanvasRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
 
-    renderer = new THREE.CanvasRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
 
-    document.body.appendChild(renderer.domElement);
-
-    // bind key events
-    document.onkeypress = keyPressed;
-}
-
-function animate() {
-    // note: three.js includes requestAnimationFrame shim
-    requestAnimationFrame(animate);
-    render();
-}
-
-function render() {
-    tunnel.render();
-    myPlayer.render();
-
-    renderer.render(scene, camera);
-}
-
-function keyPressed(e) {
-    var code = e.charCode;
-    if(code == 100 /* 'a' */) {
-        myPlayer.moveRight();
+        // bind key events
+        document.onkeypress = keyPressed;
     }
-    if(code == 97 /* 'a' */) {
-        myPlayer.moveLeft();
+
+    function animate() {
+        // note: three.js includes requestAnimationFrame shim
+        requestAnimationFrame(animate);
+        render();
     }
-}
+
+    function render() {
+        tunnel.render();
+        myPlayer.render();
+
+        renderer.render(scene, camera);
+    }
+
+    function keyPressed(e) {
+        var code = e.charCode;
+        if(code == 100 /* 'a' */) {
+            myPlayer.moveRight();
+        }
+        if(code == 97 /* 'a' */) {
+            myPlayer.moveLeft();
+        }
+    }
+
+    // Initialization
+    init();
+    animate();
+});
