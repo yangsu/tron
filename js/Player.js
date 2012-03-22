7f4ddc62c6944b86cd26e3110df66ac5c33c9bb7
Yang Su <suyang1+services@gmail.com>
2012-03-22 01:00:32 -0400

    spacing and stylistic issues

--- js/Player.js
+++ js/Player.js
@@ -4,22 +4,22 @@
 
 function Player(scene) {
     this.scene = scene;
-    
+
     this.materials = [
-                new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}), // right
-                new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}), // left
-                new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}), //top
-                new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}), // bottom
-                new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}), // back
-                new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}) // front
-        ];
-	//this.material  = new THREE.MeshBasicMaterial( { map: texture } );
-	
+        new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}), // right
+        new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}), // left
+        new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}), //top
+        new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}), // bottom
+        new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}), // back
+        new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture("img/t.jpg")}) // front
+    ];
+    //this.material  = new THREE.MeshBasicMaterial( { map: texture } );
+
     this.material = new THREE.MeshBasicMaterial({
         color: 0x0000FF,
         wireframe:false
     });
-	
+
     this.playerMesh = new THREE.Mesh(
         new THREE.CubeGeometry(10, 10, 20),
         this.material);
@@ -33,9 +33,8 @@ function Player(scene) {
 
 // temp method for testing lights
 Player.prototype.getZ = function(){
-	return this.playerMesh.position.z;
-}
-
+    return this.playerMesh.position.z;
+};
 
 Player.prototype.moveLeft = function () {
     this.position.theta -= this.velocity.theta;
