ecbbe854283efc885b496f51f64d47b32dbab443
Yang Su <suyang1+services@gmail.com>
2012-04-08 03:19:54 -0400

    stylistic changes

--- js/ParticleEngine.js
+++ js/ParticleEngine.js
@@ -2,10 +2,10 @@
  * @author Troy
  */
 
-function ParticleEngine(){
-    
-    this.particleCount = 800,
-    particles = new THREE.Geometry(),
+function ParticleEngine() {
+
+    this.particleCount = 800;
+    particles = new THREE.Geometry();
     pMaterial = new THREE.ParticleBasicMaterial({
         color: 0xFFFFFF,
         size: 5,
@@ -13,50 +13,51 @@ function ParticleEngine(){
         blending: THREE.AdditiveBlending,
         transparent: true
     });
-    
-    for(var p = 0;  p < this.particleCount; p++)
-    {
-        var theta = Math.random() * (2 * Math.PI),
-            radius = Math.random() * 75 + CONFIG.tunnelRadius + 10;
-            
-        var pX = radius * Math.cos(theta),
-            pY = radius * Math.sin(theta),
-            pZ = Math.random() * (-CONFIG.cameraFar*2);
-        
+
+    var p, theta, radius, pX, pY, pZ;
+    for (p = 0; p < this.particleCount; p += 1) {
+        theta = Math.random() * (2 * Math.PI);
+        radius = Math.random() * 75 + CONFIG.tunnelRadius + 10;
+
+        pX = radius * Math.cos(theta);
+        pY = radius * Math.sin(theta);
+        pZ = Math.random() * (-CONFIG.cameraFar * 2);
+
         particle = new THREE.Vertex(new THREE.Vector3(pX, pY, pZ));
         particle.velocity = new THREE.Vector3(0, 0, Math.random());
-            
+
         particles.vertices.push(particle);
     }
-    
+
     this.particleSystem = new THREE.ParticleSystem(particles, pMaterial);
     this.particleSystem.sortParticles = true;
-    
+
     window.scene.add(this.particleSystem);
 }
 
 // NEed to refactor code
 ParticleEngine.prototype.update = function () {
-  var pCount = this.particleCount;
-  while(pCount--) {
-    // get the particle
-    var particle = particles.vertices[pCount];
+    var pCount = this.particleCount,
+        particle;
+    while (pCount--) {
+        // get the particle
+        particle = particles.vertices[pCount];
 
-    // check if we need to reset
-    if( Math.abs(particle.position.z) < Math.abs(window.levelProgress) ) {
-      particle.position.z = window.levelProgress - CONFIG.cameraFar*2;
-      particle.velocity.z = 0;
+        // check if we need to reset
+        if (Math.abs(particle.position.z) < Math.abs(window.levelProgress)) {
+            particle.position.z = window.levelProgress - CONFIG.cameraFar * 2;
+            particle.velocity.z = 0;
+        }
+
+        // update the velocity with
+        // a splat of randomniz
+        particle.velocity.z += Math.random() * 0.1;
+
+        // and the position
+        particle.position.addSelf(particle.velocity);
     }
-    
-    // update the velocity with
-    // a splat of randomniz
-    particle.velocity.z += Math.random() * .1;
 
-    // and the position
-    particle.position.addSelf(particle.velocity);
-  }
-
-  // flag to the particle system
-  // that we've changed its vertices.
-  this.particleSystem.geometry.__dirtyVertices = true;
-}
+    // flag to the particle system
+    // that we've changed its vertices.
+    this.particleSystem.geometry.__dirtyVertices = true;
+};
--- js/SkyBox.js
+++ js/SkyBox.js
@@ -2,33 +2,35 @@
  * @author Troy
  */
 
-function SkyBox(){
-      
-    // http://learningthreejs.com/data/lets_do_a_sky/docs/lets_do_a_sky.html  
-    var urlPrefix   = "img/TronSkyBox/";
-    var urls = [ urlPrefix + "PosX.png", urlPrefix + "NegX.png",
-    urlPrefix + "PosY.png", urlPrefix + "NegY.png",
-    urlPrefix + "PosZ.png", urlPrefix + "NegZ.png" ];
-    
-    var textureCube = THREE.ImageUtils.loadTextureCube( urls );
-    
-    var shader  = THREE.ShaderUtils.lib["cube"];
-    shader.uniforms["tCube"].texture = textureCube;
-    
-    var material = new THREE.ShaderMaterial({
-        fragmentShader  : shader.fragmentShader,
-        vertexShader    : shader.vertexShader,
-        uniforms        : shader.uniforms
-    });
-    
-    this.skyboxMesh  = new THREE.Mesh( new THREE.CubeGeometry( 100000, 100000, 100000, 1, 1, 1, null, true ), material );
+function SkyBox() {
+    // http://learningthreejs.com/data/lets_do_a_sky/docs/lets_do_a_sky.html
+    var urlPrefix   = 'img/TronSkyBox/',
+        urls = [
+            urlPrefix + 'PosX.png',
+            urlPrefix + 'NegX.png',
+            urlPrefix + 'PosY.png',
+            urlPrefix + 'NegY.png',
+            urlPrefix + 'PosZ.png',
+            urlPrefix + 'NegZ.png'
+        ],
+
+        textureCube = THREE.ImageUtils.loadTextureCube(urls),
+        shader  = THREE.ShaderUtils.lib.cube,
+        material = new THREE.ShaderMaterial({
+            fragmentShader  : shader.fragmentShader,
+            vertexShader    : shader.vertexShader,
+            uniforms        : shader.uniforms
+        });
+
+    shader.uniforms.tCube.texture = textureCube;
+
+    this.skyboxMesh  = new THREE.Mesh(new THREE.CubeGeometry(100000, 100000, 100000, 1, 1, 1, null, true), material);
     // If you turn camera around, won't see skybox
     this.skyboxMesh.flipSided = true;
-    
-    window.scene.add( this.skyboxMesh );
+
+    window.scene.add(this.skyboxMesh);
 }
 
-SkyBox.prototype.update = function(){
+SkyBox.prototype.update = function () {
     // need to move box to keep up with position of player???
-    
-}
+};
