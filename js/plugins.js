fb5f9bd4044c78d7b0e46a954631dcab92bdf204
Yang Su <suyang1+services@gmail.com>
2012-04-28 17:05:39 -0400

    moved constants to plugins

--- js/plugins.js
+++ js/plugins.js
@@ -2,4 +2,7 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 (function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
 (function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
 
-
+// CONSTANTS
+window.PI = Math.PI;
+window.TWOPI = 2 * Math.PI;
+window.HALFPI = Math.PI / 2;
